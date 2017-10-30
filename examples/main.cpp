59281c88a083312cd4afeb49f65aff3dd07b25aa
vianney <vianney@quasardb.net>
2017-10-30 16:56:18 +0100

    Need to figure out apply.

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,5 +1,7 @@
 #include <control.hpp>
 #include <iostream>
+#include <string>
+#include <tuple>
 #include <term.hpp>
 
 constexpr int func(int a)
@@ -7,13 +9,69 @@ constexpr int func(int a)
     return 2 * a;
 }
 
+void h(int a, std::string b)
+{
+    std::cout << "a: " << a << " - b: " << b << '\n';
+}
+
+template <typename T>
+T parse(std::string arg);
+
+template <>
+int parse<int>(std::string arg)
+{
+    return std::atoi(arg.c_str());
+}
+
+template <>
+std::string parse<std::string>(std::string arg)
+{
+    return arg;
+}
+
+template <typename R, typename... Types>
+constexpr std::size_t get_function_arg_count(R (*f)(Types...))
+{
+    return sizeof...(Types);
+}
+
+template <std::size_t I, typename R, typename... Types>
+constexpr auto get_function_arg_type(R (*f)(Types...))
+{
+    auto t = std::tuple<Types...>{};
+    return std::get<I>(t);
+}
+
+template <typename F, std::size_t... I>
+void parser_impl(const std::vector<std::string> & tokens, F && f, std::index_sequence<I...>)
+{
+    f(parse<decltype(get_function_arg_type<I>(f))>(tokens.at(I + 1))...);
+}
+
+template<std::size_t N, typename F, typename Indices = std::make_index_sequence<N>>
+void parser(const std::vector<std::string> & tokens, F &&f)
+{
+    parser_impl(tokens, f, Indices{});
+}
+
+template <typename F>
+void caller(F && f, std::string to_parse)
+{
+    const auto tokens = term::internal::split(to_parse, ' ');
+    parser<get_function_arg_count(f)>(tokens, f);
+}
+
+
 int main()
 {
     std::string command{"cmd 3 str"};
+
+    caller(h, command);
+
     // constexpr auto l = [](int a) { return 2 * a; };
-    constexpr term::control ctrl{std::string_view{"asd", 3}, func};
+    // constexpr term::control ctrl{std::string_view{"asd", 3}, func};
     // constexpr int a = ctrl.call(2);
-    std::cout << ctrl.call(2) << '\n';
+    // std::cout << ctrl.call(2) << '\n';
 
     // term::term t{};
     // t.run();
--- termpp/utils.hpp
+++ termpp/utils.hpp
@@ -1,4 +1,7 @@
 #include <array>
+#include <sstream>
+#include <string>
+#include <vector>
 
 namespace term
 {
@@ -8,6 +11,18 @@ template <typename D = void, typename... T>
 constexpr auto make_array(T &&... t) -> std::array<std::conditional_t<std::is_void<D>::value, std::common_type_t<T...>, D>, sizeof...(T)>
 {
     return {{std::forward<T>(t)...}};
+}
+
+std::vector<std::string> split(const std::string & s, char delim)
+{
+    std::stringstream ss(s);
+    std::string item;
+    std::vector<std::string> elems;
+    while (std::getline(ss, item, delim))
+    {
+        elems.emplace_back(std::move(item));
+    }
+    return elems;
 }
 }
 }
