0cfe5ff93224e06f2b4d24b611b1596b99649182
vianney <vianney@quasardb.net>
2017-11-03 15:16:40 +0100

    Add several number types to parse.

--- termpp/parse.hpp
+++ termpp/parse.hpp
@@ -5,16 +5,58 @@
 namespace term
 {
 template <typename T>
-T parse(std::string arg);
+T parse(const std::string &arg);
 
 template <>
-int parse<int>(std::string arg)
+int parse<int>(const std::string &arg)
 {
-    return std::atoi(arg.c_str());
+    return std::stoi(arg);
 }
 
 template <>
-std::string parse<std::string>(std::string arg)
+long parse<long>(const std::string &arg)
+{
+    return std::stol(arg);
+}
+
+template <>
+long long parse<long long>(const std::string &arg)
+{
+    return std::stoll(arg);
+}
+
+template <>
+unsigned long parse<unsigned long>(const std::string &arg)
+{
+    return std::stoul(arg);
+}
+
+template <>
+unsigned long long parse<unsigned long long>(const std::string &arg)
+{
+    return std::stoull(arg);
+}
+
+template <>
+float parse<float>(const std::string &arg)
+{
+    return std::stof(arg);
+}
+
+template <>
+double parse<double>(const std::string &arg)
+{
+    return std::stod(arg);
+}
+
+template <>
+long double parse<long double>(const std::string &arg)
+{
+    return std::stold(arg);
+}
+
+template <>
+std::string parse<std::string>(const std::string &arg)
 {
     return arg;
 }
